f90d93cf772ebad361f3003602e2b8da65c77177
Manuel Thalmann <m@nuth.ch>
2021-12-23 11:28:35 +0100

    Decode the text using the default encoding

--- res/win32_get_clipboard_text_html.ps1
+++ res/win32_get_clipboard_text_html.ps1
@@ -7,6 +7,6 @@ foreach ($property in $properties) {
     $metadata[$property.Groups[1].Value] = $property.Groups[2].Value
 }
 
-$text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::UTF8.GetBytes($content)[$metadata["StartFragment"]..$($metadata["EndFragment"] - 1)])
+$text = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($content)[$metadata["StartFragment"]..$($metadata["EndFragment"] - 1)])
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::WriteLine($text)
