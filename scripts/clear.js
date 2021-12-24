c020fe32ea6443d53728969e2a4bb7d18c19f683
Slava Knyazev <slava@knyazev.io>
2021-12-24 00:02:44 -0500

    Format

--- scripts/clear.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var shelljs_1 = require("shelljs");
-shelljs_1.rm('-rf', "out/");
-shelljs_1.rm('-rf', "out_test/");
--- src/language_detection.ts
+++ src/language_detection.ts
@@ -1,95 +1,97 @@
-
-import { ModelOperations, ModelResult} from "@vscode/vscode-languagedetection";
+import { ModelOperations, ModelResult } from "@vscode/vscode-languagedetection";
 
 class LanguageDetection {
-	private static readonly expectedRelativeConfidence = 0.001;
+  private static readonly expectedRelativeConfidence = 0.001;
 
-	private _loadFailed = false;
-	private _modelOperations: ModelOperations | undefined;
+  private _loadFailed = false;
+  private _modelOperations: ModelOperations | undefined;
 
-	private async getModelOperations(): Promise<ModelOperations> {
-		if (this._modelOperations) {
-			return this._modelOperations;
-		}
+  private async getModelOperations(): Promise<ModelOperations> {
+    if (this._modelOperations) {
+      return this._modelOperations;
+    }
 
-		this._modelOperations = new ModelOperations();
-        return this._modelOperations;
-	}
+    this._modelOperations = new ModelOperations();
+    return this._modelOperations;
+  }
 
-	async detectLanguage(content: string ): Promise<string | undefined> {
+  async detectLanguage(content: string): Promise<string | undefined> {
+    if (content) {
+      for await (const language of this.detectLanguagesImpl(content)) {
+        return language;
+      }
+    }
+    return "";
+  }
 
-		if (content) {
-			for await (const language of this.detectLanguagesImpl(content)) {
-				return language;
-			}
-		}
-		return '';
-	}
+  async detectLanguages(content: string): Promise<string[]> {
+    const languages: string[] = [];
+    if (content) {
+      for await (const language of this.detectLanguagesImpl(content)) {
+        languages.push(language);
+      }
+    }
+    return languages;
+  }
 
-	async detectLanguages(content: string): Promise<string[]> {
-		const languages: string[] = [];
-		if (content) {
-			for await (const language of this.detectLanguagesImpl(content)) {
-				languages.push(language);
-			}
-		}
-		return languages;
-	}
+  private async *detectLanguagesImpl(content: string) {
+    if (this._loadFailed) {
+      return;
+    }
 
-	private async * detectLanguagesImpl(content: string) {
-		if (this._loadFailed) {
-			return;
-		}
+    let modelOperations: ModelOperations | undefined;
+    try {
+      modelOperations = await this.getModelOperations();
+    } catch (e) {
+      this._loadFailed = true;
+      return;
+    }
 
-		let modelOperations: ModelOperations | undefined;
-		try {
-			modelOperations = await this.getModelOperations();
-		} catch (e) {
-			this._loadFailed = true;
-			return;
-		}
+    const modelResults = await modelOperations.runModel(content);
+    if (!modelResults) {
+      return;
+    }
 
-		const modelResults = await modelOperations.runModel(content);
-		if (!modelResults) {
-			return;
-		}
+    if (
+      modelResults[0].confidence < LanguageDetection.expectedRelativeConfidence
+    ) {
+      return;
+    }
 
-		if (modelResults[0].confidence < LanguageDetection.expectedRelativeConfidence) {
-			return;
-		}
+    const possibleLanguages: ModelResult[] = [modelResults[0]];
 
-		const possibleLanguages: ModelResult[] = [modelResults[0]];
+    for (let current of modelResults) {
+      if (current === modelResults[0]) {
+        continue;
+      }
 
-		for (let current of modelResults) {
+      const currentHighest = possibleLanguages[possibleLanguages.length - 1];
 
-			if (current === modelResults[0]) {
-				continue;
-			}
-
-			const currentHighest = possibleLanguages[possibleLanguages.length - 1];
-
-			if (currentHighest.confidence - current.confidence >= LanguageDetection.expectedRelativeConfidence) {
-				while (possibleLanguages.length) {
-					// TODO: see if there's a better way to do this.
-					const vscodeLanguageId = possibleLanguages.shift()!.languageId;
-					if (vscodeLanguageId) {
-						yield vscodeLanguageId;
-					}
-				}
-				if (current.confidence > LanguageDetection.expectedRelativeConfidence) {
-					possibleLanguages.push(current);
-					continue;
-				}
-				return;
-			} else {
-				if (current.confidence > LanguageDetection.expectedRelativeConfidence) {
-					possibleLanguages.push(current);
-					continue;
-				}
-				return;
-			}
-		}
-	}
+      if (
+        currentHighest.confidence - current.confidence >=
+        LanguageDetection.expectedRelativeConfidence
+      ) {
+        while (possibleLanguages.length) {
+          // TODO: see if there's a better way to do this.
+          const vscodeLanguageId = possibleLanguages.shift()!.languageId;
+          if (vscodeLanguageId) {
+            yield vscodeLanguageId;
+          }
+        }
+        if (current.confidence > LanguageDetection.expectedRelativeConfidence) {
+          possibleLanguages.push(current);
+          continue;
+        }
+        return;
+      } else {
+        if (current.confidence > LanguageDetection.expectedRelativeConfidence) {
+          possibleLanguages.push(current);
+          continue;
+        }
+        return;
+      }
+    }
+  }
 }
 
-export { LanguageDetection};
+export { LanguageDetection };
